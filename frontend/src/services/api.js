a2478b0d834b8f52b6317fcd7759e286c5d657af
kumar <kmritunjay525@gmail.com.com>
2026-02-12 17:25:07 +0530

    -

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-console.log(import.meta.env.VITE_API_URL);
+console.log("API URL:",import.meta.env.VITE_API_URL);
 
 export const getProjects = () => API.get("/api/projects");
 
