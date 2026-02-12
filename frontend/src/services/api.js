6cdf87d6c3552e4338730734b1d044a770adc253
kumar <kmritunjay525@gmail.com.com>
2026-02-12 17:23:36 +0530

    debugging

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,8 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+console.log(import.meta.env.VITE_API_URL);
+
 export const getProjects = () => API.get("/api/projects");
 
 // ✅ Send Contact Message
