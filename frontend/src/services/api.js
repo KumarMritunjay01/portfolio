0cd5bd4571d5bb687a9195a1858e5c4961223fce
kumar <kmritunjay525@gmail.com.com>
2026-02-12 17:54:34 +0530

    Force new build

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
+console.log("NEW BUILD TEST ");
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const getProjects = () => API.get("/api/projects");
+export const getProjects = () => {
+   console.log("Calling API → /api/projects ");
+   return API.get("/api/projects");
+};
 
 // ✅ Send Contact Message
 export const sendMessage = (data) => API.post("/api/contact", data);
