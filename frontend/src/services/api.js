499af3a30be1c385a59ad76f0bba963c24f7f386
kumar <kmritunjay525@gmail.com.com>
2026-02-12 17:28:07 +0530

    hard-proof testing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
 
 console.log("API URL:",import.meta.env.VITE_API_URL);
+const API = axios.create({
+  baseURL: "https://portfolio-lo6u.onrender.com"
+});
 
 export const getProjects = () => API.get("/api/projects");
 
