0a478e7aee0273ee94143a1309e9b5c9698a623d
kumar <kmritunjay525@gmail.com.com>
2026-02-12 17:41:07 +0530

    -

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-console.log("API URL:",import.meta.env.VITE_API_URL);
 const API = axios.create({
-  baseURL: "https://portfolio-lo6u.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const getProjects = () => API.get("/api/projects");
